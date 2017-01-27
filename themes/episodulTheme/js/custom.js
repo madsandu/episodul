56ea3b03f3c420a097675739837cf024af26a506
madsandu <madsandu@gmail.com>
2017-01-27 11:01:26 +0300

    change layout of series page

--- themes/episodulTheme/js/custom.js
+++ themes/episodulTheme/js/custom.js
@@ -21,19 +21,12 @@
     };
     
     //IMDB Rating
-//    Drupal.behaviors.IMDBrating = {
-//        attach: function (d,s,id) {
-//            var js,stags=d.getElementsByTagName(s)[0];
-//            if(d.getElementById(id)){
-//                return;
-//            }
-//            js=d.createElement(s);
-//            js.id=id;
-//            js.src="http://g-ec2.images-amazon.com/images/G/01/imdb/plugins/rating/js/rating.min.js";
-//            stags.parentNode.insertBefore(js,stags);
-//        }
-//    };
-//    (document,"script","imdb-rating-api");
+    Drupal.behaviors.IMDBrating = {
+        attach: function (d,s,id) {
+          var js, stags = d.getElementsByTagName(s)[0]; if (d.getElementById(id)){return; }js = d.createElement(s); js.id = id; js.src = "http://g-ec2.images-amazon.com/images/G/01/imdb/plugins/rating/js/rating.min.js"; stags.parentNode.insertBefore(js, stags);
+        }
+        (document, 'script', 'imdb-rating-api')
+    };
 })(jQuery);
 
 
